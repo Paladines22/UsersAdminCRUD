644384846f3b38c30a71a44ff6ee0f85025f18b6
ValentinaPaladines <val.paladinezunal@gmail.com>
2023-05-09 15:09:43 -0500

    navbar styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ const NavBar = ({ actionAddBtn }) => {
     <nav className=" w-full flex flex-row justify-around items-center mb-5 bg-gradient-to-r from-cyan-400 to-blue-500 p-10 rounded">
       <h1 className="text-4xl font-bold  text-gray-600 hover:text-gray-700 hover:cursor-pointer">
         {' '}
-        <i class="bx bxs-user-account"></i> Users Admin{' '}
+        <i class="bx bxs-user-account"></i> USER ADMIN{' '}
       </h1>
       <button
         className="bg-cyan-200 text-gray-700 p-2 cursor-wait border border-transparent hover:border-white  rounded hover:bg-cyan-600 hover:text-white "
